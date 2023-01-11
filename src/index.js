83ec5825315dc67a7c651b528397eee3ebcf0ef8
Nikolay Tyushkov <begemot@begemotov.net>
2023-01-11 18:38:25 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-// You should implement your task here.
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if (!matrix || !matrix.length) return [];
+
+  let len = 0;
+  for (const v of matrix) len += v.length;
+
+  let res = new Array(len), f = true, i = 0;
+
+  for (const v of matrix) {
+    if (f) 
+      for (const t of v) res[i++] = t;
+    else
+      for (let t = v.length - 1; t >= 0; t--) res[i++] = v[t];
+    f = !f;
+  }
+  
+  return res;
 }
